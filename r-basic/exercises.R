53cd9ee973c35534808d6dd2ac8f6d32f3a68978
kael <piskael03@gmail.com>
2018-11-08 16:28:20 -0500

    add notes and solution for r basic exercises

---
+++ r-basic/exercises.R
@@ -0,0 +1,27 @@
+# 1
+print(2^5)
+
+# 2
+stock.prices <- c(23, 27, 23, 21, 34)
+print(stock.prices)
+
+# 3
+stock.names <- c('Mon', 'Tues', 'Web', 'Thu', 'Fri')
+names(stock.prices) <- stock.names
+print(stock.prices)
+
+# 4
+avg <- mean(stock.prices)
+print(avg)
+
+#5
+over.23 <- stock.prices > 23
+print(over.23)
+
+#6
+over.23 <- stock.prices[stock.prices > 23]
+print(over.23)
+
+#7
+highest <- max(stock.prices)
+print(highest)
